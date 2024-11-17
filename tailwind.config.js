8a9d1553c0cca159d17c24489cabd6410a2e6938
Tiisu <tiisusharif@gmail.com>
2024-11-17 18:28:15 +0000

    My first Commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html",
+            "./images",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primaryColor: '#2E584E',
+        secondaryColor: '#C3F499',
+        primaryLightColor: '#3A685D',
+
+      },
+    },
+  },
+  plugins: [],
+}
+
